2e73214e7a96cbeb71f1497cc839653718ccfbe2
Ikuo Terado <eqobar@gmail.com>
2015-01-24 03:13:24 +0900

    Update dist files

--- dist/scripts/game-time-attack.js
+++ dist/scripts/game-time-attack.js
@@ -1,67 +1,62 @@
 (function() {
   'use strict';
   Polymer('game-time-attack', {
-    MAX_COUNT: 30,
-    MAX_PRECOUNT: 3,
+    INTERVAL_DURATION: 1000,
+    COUNT: 30,
+    PRECOUNT: 3,
+    NO_COUNT: 3,
     score: null,
     timer: null,
     count: null,
-    preTimer: null,
-    preCount: null,
     initialize: function() {
       return this.title = 'タイムアタック';
     },
     start: function() {
       this.score = 0;
-      this.count = this.MAX_COUNT;
-      this.tick();
-      this.preCount = this.MAX_PRECOUNT;
-      return this.preTimer = setInterval((function(_this) {
-        return function() {
-          _this.preCount--;
-          _this.tick();
-          if (_this.preCount === 0) {
-            _this.preCount = '';
-            clearInterval(_this.preTimer);
-            _this.preTimer = null;
-            return _this.doStart();
-          }
-        };
-      })(this), 1000);
-    },
-    tick: function() {
-      this.$.precount.classList.remove('tick');
-      return setTimeout((function(_this) {
-        return function() {
-          return _this.$.precount.classList.add('tick');
-        };
-      })(this), 100);
-    },
-    doStart: function() {
+      this.maxScore = localStorage['time-attack'] || 0;
+      this.timeLeft = this.COUNT;
+      this.count = this.COUNT + this.PRECOUNT + this.NO_COUNT + 1;
       return this.timer = setInterval((function(_this) {
         return function() {
+          var _ref;
+          _this.tick();
           _this.count--;
-          if (_this.count === 3) {
-            return _this.$.count.classList.add('alert');
-          } else if (_this.count === 0) {
-            return _this.over();
+          if (_this.count > _this.COUNT + _this.PRECOUNT) {
+
+          } else if ((_this.COUNT + _this.PRECOUNT >= (_ref = _this.count) && _ref > _this.COUNT)) {
+            _this.jumbotext = _this.count - _this.COUNT;
+            return window.sound.play('click');
+          } else if (_this.count === _this.COUNT) {
+            return _this.jumbotext = '';
+          } else {
+            _this.timeLeft = _this.count;
+            if (_this.count <= 5) {
+              window.sound.play('click');
+            }
+            if (_this.count <= 0) {
+              return _this.over();
+            }
           }
         };
-      })(this), 1000);
+      })(this), this.INTERVAL_DURATION);
+    },
+    tick: function() {
+      this.jumbotextClass = '';
+      return setTimeout((function(_this) {
+        return function() {
+          return _this.jumbotextClass = 'tick';
+        };
+      })(this), 0);
     },
     stop: function() {
-      if (this.preTimer != null) {
-        clearInterval(this.preTimer);
-        this.preTimer = null;
-      }
-      if (this.timer) {
+      if (this.timer != null) {
         clearInterval(this.timer);
         return this.timer = null;
       }
     },
     onHit: function(event) {
       var point, ratio;
-      if (this.timer === null) {
+      if (this.timer == null) {
         return;
       }
       point = parseInt(event.detail.point);
@@ -69,10 +64,15 @@
       return this.score += point * ratio;
     },
     over: function() {
-      clearInterval(this.timer);
-      this.timer = null;
+      var isMaxScore;
+      this.stop();
+      isMaxScore = this.score > this.maxScore;
+      if (isMaxScore) {
+        this.maxScore = this.score;
+        localStorage['time-attack'] = this.score;
+      }
       return this.finish({
-        message: "スコア: " + this.score
+        message: ("得点 : " + this.score) + (isMaxScore ? '　★本日の最高得点!★' : '')
       });
     }
   });
